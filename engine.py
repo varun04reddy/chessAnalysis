7257bd97ed37e8b901049062fb89c52156dbbebf
Varun Reddy <varun04reddy@gmail.com>
2023-12-27 18:08:24 -0500

    engine

--- engine.py
+++ engine.py
@@ -6,3 +6,24 @@ engine = chess.engine.SimpleEngine.popen_uci("C:\\Users\\varun\\UVA CS\\personal
 
 print("hello world")
 
+
+
+class Engine:
+
+    
+    def __init__(self, engine_path):
+        self.engine_path = engine_path
+        self.engine = None
+
+    def start_engine(self):
+        self.engine = chess.engine.SimpleEngine.popen_uci(self.engine_path)
+
+    def make_move(self, board):
+        if not board.is_game_over():
+            result = self.engine.play(board, chess.engine.Limit(time=0.1))
+            return result.move
+        return None
+
+    def close_engine(self):
+        if self.engine:
+            self.engine.quit()
