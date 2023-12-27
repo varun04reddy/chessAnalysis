c050c25649918cb868462a9c1ff3a295a86fc917
Varun Reddy <varun04reddy@gmail.com>
2023-12-27 18:06:45 -0500

    engine

---
+++ engine.py
@@ -0,0 +1,8 @@
+import chess
+import chess.engine
+
+
+engine = chess.engine.SimpleEngine.popen_uci("C:\\Users\\varun\\UVA CS\\personal projects\\chess\\chessAnalysis\\stockfish\\stockfish-windows-x86-64-avx2.exe")
+
+print("hello world")
+
